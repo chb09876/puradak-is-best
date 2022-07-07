3335eff416d9b6f488ad02ba0a39ddc80cf33b39
licakim <103026721+licakim@users.noreply.github.com>
2022-07-07 13:11:24 +0900

    Update queue.c

--- licakim/week1/queue.c
+++ licakim/week1/queue.c
@@ -3,20 +3,20 @@
 #include<stdlib.h>
 
 typedef struct{
- int max_size;
- int *queuearr;
- int front;
- int rear;
- int size;
+  int max_size;
+  int *queuearr;
+  int front;
+  int rear;
+  int size;
 } Queue;
 
 void init(Queue *a)
 {
- a -> max_size = 10;
- a -> queuearr= (int *)malloc(sizeof(int) * a -> max_size);
- a -> front = 0;
- a -> rear = 0;
- a -> size = 0;
+  a -> max_size = 10;
+  a -> queuearr= (int *)malloc(sizeof(int) * a -> max_size);
+  a -> front = 0;
+  a -> rear = 0;
+  a -> size = 0;
 }
 
 int Is_Empty(Queue *a)
@@ -29,40 +29,40 @@ int Is_Empty(Queue *a)
 
 void push(Queue *a, int new_data)
 {
-   	if(a->size == a->max_size - 1)
-	{
-	   int *temp;
+     if(a->size == a->max_size - 1)
+     {
+       int *temp;
        int k;
        temp = (int*)malloc(sizeof(int)*(a->max_size*2));
 	   for(k = 1; k<a->max_size; k++)
-	       temp[k] = a ->queuearr[(a->front + k)% a->max_size];
-	   a ->max_size = a -> max_size *2;
-	   free(a ->queuearr);
-	   a -> queuearr = temp;
+       temp[k] = a ->queuearr[(a->front + k)% a->max_size];
+       a -> max_size = a -> max_size *2;
+       free(a ->queuearr);
+       a -> queuearr = temp;
        a -> front = 0;
        a -> rear = k-1;
     }
-	a->rear = (a->rear +1) % (a->max_size);
+    a->rear = (a->rear +1) % (a->max_size);
     a->queuearr[a->rear] = new_data;
-	a -> size = a->size + 1;
+    a -> size = a->size + 1;
 }
 
 void pop(Queue *a)
 {
-	if(Is_Empty(a))
-	      printf("%d\n", -1);
+    if(Is_Empty(a))
+	   printf("%d\n", -1);
     else
     {
-	    a->front = (a->front + 1) % (a-> max_size);
-		printf("%d\n", a->queuearr[a->front]);
-		a ->size = a->size -1;
+	   a->front = (a->front + 1) % (a-> max_size);
+	   printf("%d\n", a->queuearr[a->front]);
+	   a ->size = a->size -1;
     }   
 }
 
 void Front(Queue *a)
 {
     if(Is_Empty(a))
-	      printf("%d\n", -1);
+	printf("%d\n", -1);
     else
         printf("%d\n",a->queuearr[(a->front)+1]);
 }
@@ -70,7 +70,7 @@ void Front(Queue *a)
 void Back(Queue *a)
 {
     if(Is_Empty(a))
-	      printf("%d\n", -1);
+	printf("%d\n", -1);
     else
         printf("%d\n",a->queuearr[a->rear]);
 

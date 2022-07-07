37f92a2a7bb5734ddf50f6872dfa1b2787aad62b
licakim <103026721+licakim@users.noreply.github.com>
2022-07-07 13:14:55 +0900

    Update stack.c

--- licakim/week1/stack.c
+++ licakim/week1/stack.c
@@ -11,17 +11,17 @@ typedef struct
 
 void init(STACK *a)
 {
-    a->top = -1;
-    a->max_size = 10;
-    a->stackarr= (int *)malloc(sizeof(int) * a->max_size);
+   a->top = -1;
+   a->max_size = 10;
+   a->stackarr= (int *)malloc(sizeof(int) * a->max_size);
 }
 
 int Is_Empty(STACK *a)
 {
-	if(a -> top == -1)
-        	return 1;
-	else
-		return 0;
+   if(a -> top == -1)
+        return 1;
+   else
+	return 0;
 }
 
 void Size(STACK *a)
@@ -31,35 +31,35 @@ void Size(STACK *a)
 
 void Top(STACK *a)
 {
-	if(Is_Empty(a))
-		printf("%d\n",-1);
-	else
+    if(Is_Empty(a))
+	   printf("%d\n",-1);
+    else
  	   printf("%d\n",a->stackarr[a->top]); //top이 가리키는 자료 출력
 }
 
 void Pop(STACK *a)
 {
-	if(Is_Empty(a))
+    if(Is_Empty(a))
 	   printf("%d\n", -1);
-	else
-	{
-		Top(a);
-		a->top = a->top - 1;
-	}
+    else
+    {
+	   Top(a);
+	   a->top = a->top - 1;
+     }
 }
 
 void Push(STACK* a, int new_data)
 {
-	if(a->top + 1 == a->max_size)
-	{
-	   int *temp;	   
+    if(a->top + 1 == a->max_size)
+    {
+       int *temp;	   
        temp = (int*)malloc(sizeof(int)* (a->max_size*2));
-	   for(int k = 0; k < a->max_size; k++)
-	      temp[k] = a ->stackarr[k];
-	   a ->max_size = a -> max_size *2;
-	   free(a -> stackarr);
-	   a ->stackarr = temp;
+       for(int k = 0; k < a->max_size; k++)
+	     temp[k] = a ->stackarr[k];
+       a ->max_size = a -> max_size *2;
+       free(a -> stackarr);
+       a ->stackarr = temp;
     }
-	 a->top = a->top + 1;
-     a->stackarr[a->top] = new_data;	  
+    a->top = a->top + 1;
+    a->stackarr[a->top] = new_data;	  
 }
